b870bd549dd3bd6b0e6ecf5ca4aecaf11a425356
bknight-i3drobotics <bknight@i3drobotics.com>
2020-02-17 13:03:57 +0000

    fix generate tfrecord script to allow different labels

--- models/research/object_detection/generate_tfrecord.py
+++ models/research/object_detection/generate_tfrecord.py
@@ -15,29 +15,33 @@ import io
 import sys
 import pandas as pd
 import tensorflow as tf
+from google.protobuf import text_format as pbtf
 
 # This is needed since the notebook is stored in the object_detection folder. 
 sys.path.append("..") 
 
 from PIL import Image
 from object_detection.utils import dataset_util
+from object_detection.utils import label_map_util
 from collections import namedtuple, OrderedDict
 
 flags = tf.app.flags
 flags.DEFINE_string('csv_input', '', 'Path to the CSV input')
 flags.DEFINE_string('output_path', '', 'Path to output TFRecord')
+flags.DEFINE_string('labelmap', '', 'Path to labelmap.pbtxt')
 flags.DEFINE_string('image_dir', '', 'Path to images')
 FLAGS = flags.FLAGS
 
+def read_labels(filename="labelmap.pbtxt"):
+    label_map_dict = label_map_util.get_label_map_dict(filename)
+    return label_map_dict
 
 # TO-DO replace this with label map
-def class_text_to_int(row_label):
-    if row_label == 'plant':
-        return 1
-    elif row_label == 'cat':
-        return 2 
+def class_text_to_int(row_label,labels_dict):
+    if row_label in labels_dict:
+        return labels_dict[row_label]
     else:
-        None
+        return None
 
 
 def split(df, group):
@@ -46,7 +50,7 @@ def split(df, group):
     return [data(filename, gb.get_group(x)) for filename, x in zip(gb.groups.keys(), gb.groups)]
 
 
-def create_tf_example(group, path):
+def create_tf_example(group, path, labels_dict):
     with tf.gfile.GFile(os.path.join(path, '{}'.format(group.filename)), 'rb') as fid:
         encoded_jpg = fid.read()
     encoded_jpg_io = io.BytesIO(encoded_jpg)
@@ -68,7 +72,7 @@ def create_tf_example(group, path):
         ymins.append(row['ymin'] / height)
         ymaxs.append(row['ymax'] / height)
         classes_text.append(row['class'].encode('utf8'))
-        classes.append(class_text_to_int(row['class']))
+        classes.append(class_text_to_int(row['class'],labels_dict))
 
     tf_example = tf.train.Example(features=tf.train.Features(feature={
         'image/height': dataset_util.int64_feature(height),
@@ -88,12 +92,14 @@ def create_tf_example(group, path):
 
 
 def main(_):
+    labels_dict = read_labels(FLAGS.labelmap)
+
     writer = tf.python_io.TFRecordWriter(FLAGS.output_path)
     path = os.path.join(FLAGS.image_dir)
     examples = pd.read_csv(FLAGS.csv_input)
     grouped = split(examples, 'filename')
     for group in grouped:
-        tf_example = create_tf_example(group, path)
+        tf_example = create_tf_example(group, path, labels_dict)
         writer.write(tf_example.SerializeToString())
 
     writer.close()
